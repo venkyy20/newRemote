4faae8695655ce37b1506da079f71b6a7e73fb2e
venkyy2019 <venkyy2019@gmail.com>
2019-09-08 11:57:34 -0400

    Added line #6

--- develop.js
+++ develop.js
@@ -3,3 +3,4 @@ This is my second line from the local machine from the develop branch locally do
 This is my third line in the file modified from the git here.
 /**********************************************************************/
 This is my first line from the develop-a branch in the develop.js file.
+  1. this is second line done through the local changes.
